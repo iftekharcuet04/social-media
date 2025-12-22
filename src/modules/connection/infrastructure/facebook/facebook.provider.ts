fcbd0fa70b28ceb3e41c4e1295064e31cde45762
orangetoolz <iftekharul@orangetoolz.com>
2025-12-22 12:22:25 +0600

    centralizing social integration

--- src/modules/connection/infrastructure/facebook/facebook.provider.ts
+++ src/modules/connection/infrastructure/facebook/facebook.provider.ts
@@ -1,29 +1,24 @@
-import { Injectable } from '@nestjs/common';
-import { OAuthProvider } from '../../domain/providers/oauth.provider';
-import { ProfileProvider } from '../../domain/providers/profile.provider';
-import { PageProvider } from '../../domain/providers/page.provider';
+import { Injectable } from "@nestjs/common";
+import { OAuthProvider } from "../../domain/providers/oauth.provider";
+import { PageProvider } from "../../domain/providers/page.provider";
+import { ProfileProvider } from "../../domain/providers/profile.provider";
 
 @Injectable()
 export class FacebookProvider
-  implements OAuthProvider, ProfileProvider, PageProvider {
-
-  async exchangeCode(dto: {
-    code: string;
-    redirectUri: string;
-  }) {
-    // call Facebook OAuth API
+  implements OAuthProvider, ProfileProvider, PageProvider
+{
+  async exchangeCode(dto: { code: string; redirectUri: string }) {
     return {
-      accessToken: 'fb_access_token',
-      expiresIn: 3600
+      accessToken: "fb_access_token",
+      expiresIn: 3600,
     };
   }
 
   async getProfile(accessToken: string) {
-    // call Graph API /me
     return {
-      id: 'fb_user_id',
-      email: 'user@facebook.com',
-      name: 'FB User'
+      id: "fb_user_id",
+      email: "user@facebook.com",
+      name: "FB User",
     };
   }
 
@@ -31,10 +26,10 @@ export class FacebookProvider
     // call Graph API /me/accounts
     return [
       {
-        id: 'page_1',
-        name: 'My Page',
-        accessToken: 'page_access_token'
-      }
+        id: "page_1",
+        name: "My Page",
+        accessToken: "page_access_token",
+      },
     ];
   }
 }

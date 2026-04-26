bb52484731c4010262085f7fae43c6156f272898
orangetoolz <iftekharul@orangetoolz.com>
2026-04-26 11:17:26 +0600

    fix: resolve missing module dependencies for social platforms

--- src/modules/facebook/facebook.module.ts
+++ src/modules/facebook/facebook.module.ts
@@ -2,8 +2,11 @@ import { Module } from '@nestjs/common';
 import { FacebookGraphClient } from './facebook-graph.client';
 import { FacebookAuthService } from './facebook.auth.service';
 import { FacebookPostStrategy } from './facebook-post.strategy';
+import { HttpModule } from '@nestjs/axios';
+import { RepositoryModule } from '../../repositories/repository.module';
 
 @Module({
+  imports: [HttpModule, RepositoryModule],
   providers: [FacebookGraphClient, FacebookAuthService, FacebookPostStrategy],
   exports: [FacebookAuthService, FacebookGraphClient, FacebookPostStrategy],
 })
--- src/modules/instagram/instagram.module.ts
+++ src/modules/instagram/instagram.module.ts
@@ -2,8 +2,11 @@ import { Module } from '@nestjs/common';
 import { InstagramGraphApiClient } from './instagram-graph.api';
 import { InstagramAuthService } from './instagram.auth.service';
 import { InstagramPostStrategy } from './instagram-post.strategy';
+import { HttpModule } from '@nestjs/axios';
+import { RepositoryModule } from '../../repositories/repository.module';
 
 @Module({
+  imports: [HttpModule, RepositoryModule],
   providers: [InstagramGraphApiClient, InstagramAuthService, InstagramPostStrategy],
   exports: [InstagramAuthService, InstagramGraphApiClient, InstagramPostStrategy],
 })
